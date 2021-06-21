16ddf72567f270937e13e84251cf995ff46d94ba
Jaimy Koster <43574560+RekCus@users.noreply.github.com>
2021-06-21 21:02:24 +0200

    update

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -89,6 +89,6 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
-        Gizmos.DrawWireSphere(transform.position, attackRadius);
+        Gizmos.DrawWireSphere(swordSlash.transform.position, attackRadius);
     }
 }
